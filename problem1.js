3037643b3cdd35f96baad2def51ac575357e7d1e
Shelvy Murphy-Brown <shelvymurphy-brown@Shelvys-MacBook-Air.local>
2021-11-10 19:27:26 -0600

    Initial commit.

---
+++ problem1.js
@@ -0,0 +1,8 @@
+
+
+const btn = document.querySelector("button");
+const name = document.getElementById("name").style.backgroundColor = "red";
+
+btn.addEventListener("click", function () {
+	name.style.backgroundColor = "red"
+});
